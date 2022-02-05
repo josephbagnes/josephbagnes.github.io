9e52b0fa3d745b1107c57ce1c71f8ea4daf26279
josephbagnes <jbrbagnes@ymail.com>
2022-02-06 07:30:02 +0800

    Update index.js
    js change

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function() {
     $('#yearSpan').text((new Date()).getFullYear());
     $('#bdaySpan').text((new Date()).getFullYear() - 1989);
+    $('#yearsOfExperienceSpan').text((new Date()).getFullYear() - 2010);
 });
